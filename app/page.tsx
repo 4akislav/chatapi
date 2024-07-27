b82777a2300df0bb067ec0ea50c34b66d74369d7
4akislav <andron2588@gmail.com>
2024-07-27 19:11:32 +0300

    chat field added

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,23 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <div>s</div>
+      <div className="styles.right">
+        <div className="styles.profileSection">
+          {/* Місце для профілю */}
+          <h2>Profile</h2>
+        </div>
+        <div className="styles.chatSection">
+          {/* Місце для чату */}
+          <h2>Chat</h2>
+        </div>
+        <div className="inputWrapper">
+          <input
+            type="text"
+            placeholder="Type a message"
+            className="styles.input"
+          />
+        </div>
+      </div>
     </main>
   );
 }
